6c1fe253122d264e3e28252f0d9043984a034925
John H Moore <john@jhmwebdesign.com>
2013-07-20 12:54:58 -0400

    created bubblesort

---
+++ src/algorithms/BubbleSort.java
@@ -0,0 +1,34 @@
+package algorithms;
+import java.util.Arrays;
+
+import ListTools.SortingList;
+
+public class BubbleSort {
+	
+	public static void bubble(int[] myArray){
+		int temp;
+		int inner;
+		int outer;
+		for (outer = myArray.length; outer > 0; outer--){
+			for (inner = 0; inner < outer-1; inner++){
+				if (myArray[inner] > myArray[inner+1]){
+					temp = myArray[inner];
+					myArray[inner] = myArray[inner+1];
+					myArray[inner+1] = temp;
+				}
+			}
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		SortingList test = new SortingList();
+		int[] testList = test.getList();
+		System.out.println(Arrays.toString(testList));
+		System.out.println(test.checkSort());
+		bubble(testList);
+		System.out.println(Arrays.toString(testList));
+		System.out.println(test.checkSort());
+	}
+
+}
