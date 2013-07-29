2f7bbfd3fcc1d14f1209fe26b1b410be2ef2b366
John H Moore <john@jhmwebdesign.com>
2013-07-29 15:20:56 -0400

    Commented SortingList source

--- src/ListTools/SortingList.java
+++ src/ListTools/SortingList.java
@@ -1,23 +1,59 @@
 package ListTools;
+
 import java.util.Random;
 import java.util.Arrays;
 
+/**
+ * 
+ * @author John H Moore	  john@jhmwebdesign.com
+ *
+ * The main purpose of the SortingList class is primarily educational (for me).
+ * Everything done in the SortingList class could be done simply by copying and pasting
+ * a few code snippets into the various sorting algorithm classes for testing purposes.
+ * Instead, I'm using SortingLists as a chance to practice working with multiple classes
+ * and packages in one project.
+ *
+ */
 public class SortingList {
+	/**
+	 * len -> length of the SortingList's numList
+	 * numList -> an array of integers
+	 * myMin -> user-defined minimum value
+	 * myMax -> user-defined maximum value
+	 */
 	private int len;
 	private int[] numList;
 	int myMin;
 	int myMax;
 	
+	/**
+	 * Default constructor
+	 * len = 100
+	 * myMin = 0
+	 * myMax = 100
+	 */
 	public SortingList(){
 		len = 100;
 		numList = populateList(len, 0, 100);
 	}
 	
+	/**
+	 * 
+	 * @param numElems -> len
+	 * myMin = 0
+	 * myMax = 100
+	 */
 	public SortingList(int numElems){
 		len = numElems;
 		numList = populateList(len, 0, 100);
 	}
 	
+	/**
+	 * 
+	 * @param numElems -> len
+	 * @param min -> myMin
+	 * @param max -> myMax
+	 */
 	public SortingList(int numElems, int min, int max){
 		len = numElems;
 		myMin = min;
@@ -25,6 +61,13 @@ public class SortingList {
 		numList = populateList(len, myMin, myMax);
 	}
 	
+	/**
+	 * Populates numList using numElems, min, and max
+	 * @param numElems
+	 * @param min
+	 * @param max
+	 * @return l (populated numList)
+	 */
 	private static int[] populateList(int numElems, int min, int max){
 		Random randNum = new Random();
 		int[] l = new int[numElems];
@@ -34,17 +77,33 @@ public class SortingList {
 		return l;
 	}
 	
+	/**
+	 * toString() method
+	 */
 	public String toString(){
 		return Arrays.toString(this.numList);
 	}
+	
+	/**
+	 * 
+	 * @return numList
+	 */
 	public int[] getList(){
 		return this.numList;
 	}
 	
+	/**
+	 * 
+	 * @return len
+	 */
 	public int getLen(){
 		return this.len;
 	}
 	
+	/**
+	 * 
+	 * @return true if sorted, false if not sorted
+	 */
 	public boolean checkSort(){
 		for (int i = 1; i < this.len; i++){
 			if (this.numList[i] < this.numList[i-1]){
